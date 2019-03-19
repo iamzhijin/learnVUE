d8dab8697f053f2570dfe44ed79d6483601f1fc9
lizhijin <lizhijin@yscredit.com>
2019-03-19 15:38:02 +0800

    test

--- firstProject/src/components/HelloWorld.vue
+++ firstProject/src/components/HelloWorld.vue
@@ -3,9 +3,23 @@
     {{ "hello" }}
     <p>{{ msg }}</p>
     <p>{{ boolean ? 'dd' : 'ff' }}</p>
-    <p v-html=htmlMsp></p>
-    <p v-text=htmlMsp></p>
-    <a v-bind:href=url>v-bind</a>
+    <p v-html="htmlMsp"></p>
+    <p v-text="htmlMsp"></p>
+    <a v-bind:href="url">v-bind</a>
+    <p v-if="boolean"> can you see me?</p>
+    <div>
+      <ul>
+        <li v-for="(item, index) in lists" :key="index">{{item}}</li>
+      </ul>
+    </div>
+    <div>
+      <button v-on:click="num+=1"> add 1 </button>
+      <span>{{ num }}</span>
+      <button v-on:click="greet">greet</button>
+      <input type="text" v-on:keyup.enter="showAlert">
+      <button v-on:click="addarr">addttr</button>
+    </div>
+    <p>dadadad</p>
   </div>
 </template>
 
@@ -17,7 +31,22 @@ export default {
       msg: 'Welcome to Your Vue.js App',
       htmlMsp: '<p>v-html</p>',
       boolean: true,
-      url: "#"
+      url: "#",
+      boolean: true,
+      lists: ['da', 'xiao'],
+      num: 1,
+      name: 'fuck',
+    }
+  },
+  methods: {
+    greet: function(){
+      alert('hello ' + this.name)
+    },
+    showAlert: function(){
+      alert('key up on enter')
+    },
+    addarr(){
+      this.lists.push("dada");
     }
   }
 }
---
+++ firstProject/src/components/Practice1.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>aaa</p>    
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Practice1',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
